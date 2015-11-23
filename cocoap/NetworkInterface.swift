355cf3b090df05dd7bd00dea8e8fc2f3c5561f71
Tomoyuki Sahara <sahara@caddr.net>
2015-11-23 19:32:05 +0900

    monitor changes.

--- cocoap/NetworkInterface.swift
+++ cocoap/NetworkInterface.swift
@@ -11,9 +11,18 @@ import SystemConfiguration
 
 class NetworkInterface {
     class func list() -> [String] {
-        let store = SCDynamicStoreCreate(nil, "Playd", nil, nil)
+        func callback(store: SCDynamicStore, array: CFArray, context: UnsafeMutablePointer<Void>) -> Void {
+            print("callbacked: \(array)")
+        }
+
+        let store = SCDynamicStoreCreate(nil, NSBundle.mainBundle().bundleIdentifier!, callback, nil)!
+        let keys = [ SCDynamicStoreKeyCreateNetworkInterface(nil, kSCDynamicStoreDomainState) ]
+        SCDynamicStoreSetNotificationKeys(store, keys, nil)
+        CFRunLoopAddSource(CFRunLoopGetCurrent(), SCDynamicStoreCreateRunLoopSource(nil, store, 0), kCFRunLoopDefaultMode)
+        
         let val = SCDynamicStoreCopyValue(store, "State:/Network/Interface")
         guard (val != nil) else { return [] }
         return (val!["Interfaces"] as! [String]).sort()
     }
 }
+
